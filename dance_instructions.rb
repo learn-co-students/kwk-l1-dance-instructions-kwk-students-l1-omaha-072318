55c59af75e76ea56171d8998170d5b3660fe63ac
bmudhapati <bmudhapati957@millardps.org>
2018-07-24 19:46:15 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,24 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loos"
+end
+
+def skip_step(left)
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left and Left"
+end
+
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+end
+
+def bob_the_reins
+  puts "Holding your arms out"
+  puts "Cross your right wrist"
+  puts "Lift your arms up and down"
+end
+
+
