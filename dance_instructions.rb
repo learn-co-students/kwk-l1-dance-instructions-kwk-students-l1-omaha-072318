f678e542dde1e3759c54213acf261f694e349941
art123craft <cammiebluecammie@gmail.com>
2018-07-26 21:04:59 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,53 @@
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+  puts " lift right foot."
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward."
+end
+starting_stance
 
-# Code your methods below
+def skip_step(left)
+  puts "Briefly"
+  puts "Lower left foot to the ground."
+  puts "Bounce left foot back up slighty, kicking it a few inches back."
+  puts "Left, right, Left and Left."
+end
+ skip_step("left")
+
+def base_footwork
+  puts "Return right foot."
+  puts "Lift right foot."
+end
+base_footwork 
+
+def bob_the_reins
+  puts "Do"
+  puts "Holding your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+bob_the_reins
+
+def lasso
+  puts "Do"
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+lasso
+
+ def bust_a_move
+   puts "Be cool and be snazzy."
+   puts "Be cool."
+   puts "Be snazzy!"
+ end
+ bust_a_move
+
+# def skip_step("right")
+#   puts "Lower rightfoot to the ground."
+#   puts "Bounce right foot back up slighty, kicking it a few inches back."
+#   puts "Right, left, Right and Right."
+# end
+# skip_step("right")
+
+  
